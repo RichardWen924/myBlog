5cd74affdaa3b2d321754ab033d0992703df13ae
wen rz <wenrz@tsintergy.com>
2026-08-09 17:20:31 +0800

    style: align blog archive left

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
     description: z.string(),
     date: z.coerce.date(),
     tags: z.array(z.string()).default([]),
+    image: z.string().optional(),
     draft: z.boolean().default(false),
   }),
 });
